4a723e26a9e26ceabf7aa3ad32c19456ff3ba83a
Dale Mckeown <dalemac.09@googlemail.com>
2012-06-07 10:21:16 +0100

    set up definitions

--- bebop.php
+++ bebop.php
@@ -8,4 +8,31 @@ Author: Dale Mckeown
 Author URI: http://phone.online.lincoln.ac.uk/dmckeown
 License: TBA
 */
+
+// This plugin is intended for BuddyPress only.
+// http://buddypress.org/
+//
+
+/*****************************************************************************
+** This program is distributed WITHOUT ANY WARRANTY; without even the 		**
+** implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. **
+*****************************************************************************/
+
+
+//initialise Bebop
+function bebop_init() {
+	
+	//Define plugin version
+	define('BP_BEBOP_VERSION', '0.1');
+	//define('BP_BEBOP_IS_INSTALLED', 1);
+	
+	//init database
+	
+	//init settings
+	
+	//include files from core.
+	include_once('core/core.php');
+}
+    
+    
 ?>
