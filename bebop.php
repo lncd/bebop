156447cd3e56abd43540a11656303cbe6786b73e
Dale Mckeown <dalemac.09@googlemail.com>
2012-06-07 09:45:09 +0100

    bebop test ]

---
+++ bebop.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Bebop
+Plugin URI: http://bebop.blogs.lincoln.ac.uk/
+Description: Bebop is the name of a rapid innovation project funded by the Joint Information Systems Committee. The project involved the utilisation of OER's from 3rd party providers such as JORUM and slideshare.
+Version: 0.1
+Author: Dale Mckeown
+Author URI: http://phone.online.lincoln.ac.uk/dmckeown
+License: TBA
+*/
+?>
