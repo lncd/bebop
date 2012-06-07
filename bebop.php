7711faa3ae2586dbc50937a1bc54302a0e82e8ad
Dale Mckeown <dalemac.09@googlemail.com>
2012-06-07 10:52:16 +0100

    added a bit of bebop initialisation code.

--- bebop.php
+++ bebop.php
@@ -2,16 +2,15 @@
 /*
 Plugin Name: Bebop
 Plugin URI: http://bebop.blogs.lincoln.ac.uk/
-Description: Bebop is the name of a rapid innovation project funded by JISC. The project involved the utilisation of OER's from 3rd party providers such as JORUM and slideshare.
+Description: Bebop is the name of a rapid innovation project funded by the Joint Information Systems Committee (JISC). The project involves the utilisation of OER's from 3rd party providers such as JORUM and slideshare.
 Version: 0.1
 Author: Dale Mckeown
 Author URI: http://phone.online.lincoln.ac.uk/dmckeown
 License: TBA
 */
 
-// This plugin is intended for BuddyPress only.
+// This plugin is intended for use on BuddyPress only.
 // http://buddypress.org/
-//
 
 /*****************************************************************************
 ** This program is distributed WITHOUT ANY WARRANTY; without even the 		**
@@ -26,13 +25,52 @@ function bebop_init() {
 	define('BP_BEBOP_VERSION', '0.1');
 	//define('BP_BEBOP_IS_INSTALLED', 1);
 	
-	//init database
+	//init database - used as a log
+	bebop_init_database();
 	
 	//init settings
+	bebop_init_settings();
+	
+	//load languages
+	bebop_init_languages();
 	
 	//include files from core.
 	include_once('core/core.php');
 }
-    
+
+//Create the log database if it does not already exist.
+function bebop_init_database() {
+	if( ! get_site_option("bebop_installed_version")) {
+		global $wpdb;
+		
+		$bebop_table_sql = "CREATE TABLE IF NOT EXISTS " . $wpdb->base_prefix . "bebop_log (
+			'id' int(10) NOT NULL auto_increment,
+			'date' timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+			'type' text NOT NULL,
+			'message' text NOT NULL,
+			PRIMARY KEY  ('id')
+		);";
+		
+		//run the query.
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($bebop_table_sql);
+		
+		//update the installed version.
+		update_site_option("bebop_installed_version", BP_BEBOP_VERSION);
+		
+		//cleanup
+		unset($bebop_table_sql);
+	}
+}
+
+function bebop_init_settings() {
+	//not currently implemented
+}
+function bebop_init_languages() {
+	//not currently implemented
+}
+
+//hook into bp_init to start bebop. 
+add_action('bp_init', 'bebop_init', 4);
     
 ?>
