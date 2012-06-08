b51ef50b30bee991c89ea4763e3c88042369b38f
Dale Mckeown <dalemac.09@googlemail.com>
2012-06-08 11:23:01 +0100

    big changes - mostly fixed. Jenkins still playing up.

--- bebop.php
+++ bebop.php
@@ -17,6 +17,7 @@ License: TBA
 ** implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. **
 *****************************************************************************/
 
+
 //initialise Bebop
 function bebop_init() {
 
@@ -39,20 +40,40 @@ function bebop_init() {
 //Code that should be fired when he plugin is activated.
 function bebop_activate() {
 	
-//Database table stuffs
-global $wpdb;
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-
-        $buddystreamSql = "CREATE TABLE IF NOT EXISTS " . $wpdb->base_prefix . "bp_bebop_log (
-          'id' int(11) NOT NULL auto_increment,
-          'date' timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-          'type' text NOT NULL,
-          'message' text NOT NULL,
-          PRIMARY KEY  ('id')
-        );";
-
-        dbDelta($buddystreamSql);
-        unset($buddystreamSql);
+	//Database table stuffs
+	global $wpdb;
+	
+	//define table sql
+    $bebop_table_log = "CREATE TABLE IF NOT EXISTS " . $wpdb->base_prefix . "bp_bebop_log ( 
+    	id int(10) NOT NULL auto_increment,
+    	date timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    	type text NOT NULL,
+    	message text NOT NULL,
+    	PRIMARY KEY  (id)
+    );";
+	$bebop_table_data = "CREATE TABLE IF NOT EXISTS " . $wpdb->base_prefix . "bp_bebop_data ( 
+    	id int(10) NOT NULL auto_increment,
+    	date timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    	option_name text NOT NULL,
+    	option_value text NOT NULL,
+    	PRIMARY KEY  (option_name)
+    );";       
+	//run queries
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+	dbDelta($bebop_table_log);    
+	dbDelta($bebop_table_data);
+	
+	//tests
+	$sql_test = $wpdb->insert( $wpdb->base_prefix . "bp_bebop_log", array( 'type' => 'test type', 'message' => 'test message' ) );
+	$sql_test = $wpdb->insert( $wpdb->base_prefix . "bp_bebop_data", array( 'option_name' => 'test option', 'option_value' => 'test value' ) );
+	
+	//save the installed version.
+	$sql_test = $wpdb->insert( $wpdb->base_prefix . "bp_bebop_data", array( 'option_name' => 'bebop_installed_version', 'option_value' => constant('BP_BEBOP_VERSION') ) );
+	
+	
+	//cleanup
+	unset($bebop_table_log);
+    unset($bebop_table_data);
 }
 
 function bebop_init_settings() {
@@ -62,12 +83,12 @@ function bebop_init_languages() {
 	//not currently implemented
 }
 //remove the tables upon deactivation
-/*function bebop_deactivate() {
+function bebop_deactivate() {
 	global $wpdb;
 	
-	$bebop_log_sql = $wpdb->query("DROP TABLE IF EXISTS bebop_log");
-	$bebop_data_sql = $wpdb->query("DROP TABLE IF EXISTS bebop_date");
-}*/
+	$wpdb->query("DROP TABLE IF EXISTS " . $wpdb->base_prefix . "bp_bebop_log");
+	$wpdb->query("DROP TABLE IF EXISTS " . $wpdb->base_prefix . "bp_bebop_data");
+}
 
 //hook into bp_init to start bebop. 
 add_action( 'bp_init', 'bebop_init', 4 );
@@ -75,6 +96,6 @@ add_action( 'bp_init', 'bebop_init', 4 );
 //init tables when the plugin is activated.
 register_activation_hook( __FILE__, 'bebop_activate' );
 
-//register_deactivation_hook( __FILE__, 'bebop_deactivate' );
+register_deactivation_hook( __FILE__, 'bebop_deactivate' );
     
 ?>
