b941823397a765b9bb538568cdb2dbf2774011ed
DaleMckeown <@dalemac.09@googlemail.com>
2012-06-11 11:20:03 +0100

    arghwth

--- core/bebop_core.php
+++ core/bebop_core.php
@@ -3,8 +3,18 @@
 
 class bebop
 {
-	function log_error($error_type, $error_message, $stream=null) {
+	//function to log errors into error table
+	function log_error($feed_id=null, $error_type, $error_message) {
+		if($feed_id) {
+			$wpdb->query( $wpdb->prepare( "INSERT INTO " . $wpdb->base_prefix . "bp_bebop_error_log (feed_id, error_type, error_message) VALUES (%s, %s, %s)", $wpdb->escape($feed_id), $wpdb->escape($error_type), $wpdb->escape($error_message) ) );
+		}
+		else {
+			$wpdb->query( $wpdb->prepare( "INSERT INTO " . $wpdb->base_prefix . "bp_bebop_error_log (feed_id, error_type, error_message) VALUES (NULL, %s, %s)", $wpdb->escape($feed_id), $wpdb->escape($error_type), $wpdb->escape($error_message) ) );
+		}
+		
 	}
+}
 
-	
+$bebop = new bebop();
+//$bebop->log_error('123', 'test error', 'test error message');
 ?>
