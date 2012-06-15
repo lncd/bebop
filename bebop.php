549f4bf89646d9951b11bdb48ed97f40a258732b
DaleMckeown <@dalemac.09@googlemail.com>
2012-06-15 11:56:13 +0100

    moved again

--- bebop.php
+++ bebop.php
@@ -17,9 +17,9 @@ License: TBA
 ** implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. **
 *****************************************************************************/
 
-
 //initialise Bebop
 function bebop_init() {
+	bebop_create_tables();
 	
 	//init settings
 	bebop_init_settings();
@@ -44,62 +44,64 @@ function bebop_init_languages() {
 }
 
 //Code that should be fired when he plugin is activated.
-function bebop_activate() {
+function bebop_create_tables() {
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	if( is_plugin_active('buddypress/bp-loader.php') ) {
+		//Database table stuffs
+		global $wpdb;
+		var_dump(is_plugin_active('buddypress/bp-loader.php'));
+		
+		//define table sql
+		$bebop_error_log = "CREATE TABLE IF NOT EXISTS " . $wpdb->base_prefix . "bp_bebop_error_log ( 
+	    	id int(10) NOT NULL auto_increment PRIMARY KEY,
+	    	feed_id int(10) NULL,
+	    	timestamp timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+	    	error_type varchar(30) NOT NULL,
+	    	error_message varchar(255) NOT NULL
+	    );";
+	    $bebop_general_log = "CREATE TABLE IF NOT EXISTS " . $wpdb->base_prefix . "bp_bebop_general_log ( 
+	    	id int(10) NOT NULL auto_increment PRIMARY KEY,
+	    	timestamp timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+	    	type varchar(20) NOT NULL,
+	    	message varchar(255) NOT NULL
+	    );";
+		
+		$bebop_options = "CREATE TABLE IF NOT EXISTS " . $wpdb->base_prefix . "bp_bebop_options ( 
+	    	timestamp timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+	    	option_name varchar(30) NOT NULL PRIMARY KEY,
+	    	option_value varchar(30) NOT NULL
+	    );";       
+		//run queries
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta($bebop_general_log);
+		dbDelta($bebop_error_log);   
+		dbDelta($bebop_options);
 	
-	//Database table stuffs
-	global $wpdb;
-	
-	//define table sql
-	$bebop_error_log = "CREATE TABLE IF NOT EXISTS " . $wpdb->base_prefix . "bp_bebop_error_log ( 
-    	id int(10) NOT NULL auto_increment PRIMARY KEY,
-    	feed_id int(10) NULL,
-    	timestamp timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-    	error_type varchar(30) NOT NULL,
-    	error_message varchar(255) NOT NULL
-    );";
-    $bebop_general_log = "CREATE TABLE IF NOT EXISTS " . $wpdb->base_prefix . "bp_bebop_general_log ( 
-    	id int(10) NOT NULL auto_increment PRIMARY KEY,
-    	timestamp timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-    	type varchar(20) NOT NULL,
-    	message varchar(255) NOT NULL
-    );";
-	
-	$bebop_options = "CREATE TABLE IF NOT EXISTS " . $wpdb->base_prefix . "bp_bebop_options ( 
-    	timestamp timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-    	option_name varchar(30) NOT NULL PRIMARY KEY,
-    	option_value varchar(30) NOT NULL
-    );";       
-	//run queries
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-	dbDelta($bebop_general_log);
-	dbDelta($bebop_error_log);   
-	dbDelta($bebop_options);
-
-	//cleanup
-	unset($bebop_general_log);
-    unset($bebop_error_log);
-    unset($bebop_options);
+		//cleanup
+		unset($bebop_general_log);
+	    unset($bebop_error_log);
+	    unset($bebop_options);
+	}
+	else {
+		add_action('admin_notices', 'my_admin_notice');
+	}
 }
-//remove the tables upon deactivation
-function bebop_deactivate() {
-	global $wpdb;
-	
-	$wpdb->query("DROP TABLE IF EXISTS " . $wpdb->base_prefix . "bp_bebop_general_log");
-	$wpdb->query("DROP TABLE IF EXISTS " . $wpdb->base_prefix . "bp_bebop_error_log");
-	$wpdb->query("DROP TABLE IF EXISTS " . $wpdb->base_prefix . "bp_bebop_options");
+	    
+function my_admin_notice(){
+	echo '<div class="updated">
+	<p>This notice only appears on the plugins page.</p>
+	</div>';
 }
 //definitions
 
 //define('WP_DEBUG', true);
 define('BP_BEBOP_VERSION', '0.1');
-//define('BP_BEBOP_IS_INSTALLED', 1);
-
+define('BP_BEBOP_IS_INSTALLED', 1);
+define( 'BP_USE_WP_ADMIN_BAR', true );
 
 //hook into bp_init to start bebop. 
 add_action( 'bp_init', 'bebop_init', 4 );
 
-//hooks into activation and deactivation of the plugin.
-register_activation_hook( __FILE__, 'bebop_activate' );
-register_deactivation_hook( __FILE__, 'bebop_deactivate' );
 //register_uninstall_hook( __FILE__, 'bebop_deactivate' )
+
 ?>
