f0cea852c14f7c2c82ee06725334bd82a87bd236
DaleMckeown <@dalemac.09@googlemail.com>
2012-06-20 08:40:35 +0100

    updating admin templates

--- core/templates/admin_home.php
+++ core/templates/admin_home.php
@@ -1,54 +1,48 @@
 <link rel="stylesheet" href="<?php echo plugins_url() . '/bebop/core/resources/css/admin.css';?>" type="text/css"> 
 
 <?php include_once( 'admin_menu.php' ); ?>
-
-<div class="bebop_admin_box">
-    <img src="<?php echo plugins_url() . '/bebop/core/resources/images/bebop_logo.png';?>" width="100" align="left"><br>
-    Welcome to the OER plugin for BuddyPress. Developed by <a href='http://www.lncd.lincoln.ac.uk'>LNCD @ the University of Lincoln</a>.
-</div>
-
-
-<div id="dashboard-widgets-wrap">
-	<div class="metabox-holder" id="dashboard-widgets">
-		<div style="width: 49.5%;" class="postbox-container">
-			
-	        <div class="meta-box-sortables ui-sortable" id="normal-sortables">
-	            <div class="postbox">
-	                <div><h3 style="cursor:default;"><span>Latest News</span></h3>
-                    	<div class="inside" style="padding:10px;">
-	                        Bebop News       
-	                    </div>
-	                </div>
-	            </div>
-	            
-	            <div class="postbox">
-	                <div><h3 style="cursor:default;"><span>Container</span></h3>
-	                	<div class="inside" style="padding:10px;">
-	                        Container     
-	                    </div>
-	                </div>
-	            </div>
-			<!-- End normal-sortables -->
-	        </div>
-	   	<!-- End postbox-container -->
-	    </div>
-	    
-	    <div style="width: 49.5%;" class="postbox-container">
-			
-	        <div class="meta-box-sortables ui-sortable" id="normal-sortables">
-	    
-	    		<div class="postbox">
-			        <div><h3 style="cursor:default;">Support</h3>
-			            <div class="inside" style="padding:10px;">
-			                Support Description         
-			            </div>
-			        </div>
-			    </div>
-			</div>
-		</div>
-	<!-- End <div class="metabox-holder" id="dashboard-widgets"> -->
+<div id='bebop_admin_container'>
+	<div class="bebop_admin_box">
+	    <img class="bebop_logo" src="<?php echo plugins_url() . '/bebop/core/resources/images/bebop_logo.png';?>"><br>
+	    Welcome to the OER plugin for BuddyPress. Developed by <a href='http://www.lncd.lincoln.ac.uk'>LNCD @ the University of Lincoln</a>.
+	    <div class="clear"></div>
 	</div>
-
-<div class="clear"></div>
-<!-- end dashboard-widgets-wrap -->
+	
+	<div class="postbox-container">
+		
+        <div class="meta-box-sortables ui-sortable">
+            <div class="postbox">
+            	<h3>Latest News</h3>
+            	<div class="inside" style="padding:10px;">
+                    Bebop News       
+                </div>
+            </div>
+            
+            <div class="postbox">
+                <h3>Container</h3>
+            	<div class="inside" style="padding:10px;">
+                    Container     
+                </div>
+               
+            </div>
+		<!-- End normal-sortables -->
+        </div>
+   	<!-- End postbox-container -->
+    </div>
+    
+    <div class="postbox-container">
+		
+        <div class="meta-box-sortables ui-sortable">
+    
+    		<div class="postbox">
+		        <h3>Support</h3>
+	            <div class="inside" style="padding:10px;">
+	                Support Description         
+	            </div>
+		    </div>
+		</div>
+	</div>
+	
+	<div class="clear"></div>
 </div>
+<!-- end bebop_admin_container -->
--- core/templates/admin_menu.php
+++ core/templates/admin_menu.php
@@ -1,6 +1,6 @@
 <div class="bebop_admin_menu">
     <a id="bebop_admin" href="?page=bebop_admin" <?php if( ( ! isset( $_GET['settings'] ) ) && ( ! isset( $_GET['view'] ) ) ) { echo 'class="activetab"'; }?>>Admin Home</a>
-    <a id="bebop_apps" href="?page=bebop_admin&settings=apps" <?php if( isset( $_GET['settings'] ) && $_GET['settings'] == "bebop_apps" ) { echo 'class="activetab"'; }?>>Apps</a>     
+    <a id="bebop_oer_providers" href="?page=bebop_admin&settings=oer_providers" <?php if( isset( $_GET['settings'] ) && $_GET['settings'] == "oer_providers" ) { echo 'class="activetab"'; }?>>OER Providers</a>     
     <a id="bebop_general" href="?page=bebop_admin&settings=general" <?php if( isset( $_GET['settings'] ) && $_GET['settings'] == "general" ) { echo 'class="activetab"'; }?>>General Settings</a>     
     <a id="bebop_cron" href="?page=bebop_admin&settings=cron" <?php if( isset( $_GET['settings'] ) && $_GET['settings'] == "cron" ){ echo 'class="activetab"'; }?>>Cron</a>     
     <a id="bebop_error_log" href="?page=bebop_admin&view=error_log" <?php if( isset( $_GET['view'] ) && $_GET['view'] == "error_log" ){ echo 'class="activetab"'; }?>>Error Log</a>
