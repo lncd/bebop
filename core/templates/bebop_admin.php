e22ecf904c235158a52a52cf1ec1c28b82639397
DaleMckeown <@dalemac.09@googlemail.com>
2012-06-22 10:24:29 +0100

    getting rid of the red, importing twiter architecture

--- core/templates/bebop_admin.php
+++ core/templates/bebop_admin.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="<?php echo plugins_url() . '/bebop/core/resources/css/admin.css';?>" type="text/css">
 <link rel="shortcut icon" href="<?php echo plugins_url() . '/bebop/core/resources/images/bebop_icon.png';?>">
 
-<?php include_once( 'admin_menu.php' ); ?>
+<?php include_once( 'bebop_admin_menu.php' ); ?>
 <div id='bebop_admin_container'>
 
 	<div class="bebop_admin_box">
