a3b2c3f9ebd3c4c51883a7b3e75830c8e08cab8a
DaleMckeown <@dalemac.09@googlemail.com>
2012-06-27 11:20:15 +0100

    git is silly

--- core/templates/user/bebop_user_settings.php
+++ core/templates/user/bebop_user_settings.php
@@ -34,7 +34,7 @@ if(isset($_GET['oer'])){
 <br/><br/>
 
 <?php
-if(isset($_GET['oer'])){
+if( isset($_GET['oer']) ) {
 	
     include(WP_PLUGIN_DIR."/bebop/extensions/".$_GET['oer']."/templates/user_settings.php");
 }
