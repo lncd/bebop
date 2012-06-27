71ae9807f9ba2f5e1d7663e1e5334ffd6ecba6b7
DaleMckeown <@dalemac.09@googlemail.com>
2012-06-27 11:14:19 +0100

    fixed it with less code than david did. bloody counters…. :P

--- core/templates/user/bebop_user_settings.php
+++ core/templates/user/bebop_user_settings.php
@@ -1,3 +1,11 @@
+<?php
+if(isset($_GET['oer'])){
+	
+    include(WP_PLUGIN_DIR."/bebop/extensions/".$_GET['oer']."/templates/user_settings.php");
+}
+?>
+</div>
+
 <h3>
 <?php if(!isset($_GET['oer'])){
     echo "<h3>Provider</h3>";
@@ -9,27 +17,21 @@
 <div class="buddystream_album_navigation_links">
     <ul>
         <?php
-		$count_check = 0;
+
         //get the active extension
-               		
         foreach( bebop_extensions::get_extension_configs() as $extension ) {
-        	if(bebop_tables::get_option('bebop_'.$extension['name'].'_provider') == "on") {
-                	echo '<li><a href="?eor=' . $extension['name'] . '">'.ucfirst($extension['displayname']).'</a></li>';         
-                	$activeExtensions[] = $extension['name'];
-            	}
-				else {
-					$count_check++;
-				}			
-        	}
-		
-		if($count_check===count(bebop_extensions::get_extension_configs())) {
-				echo "No extensions are currently active. Please activate them in the bebop OER provides admin panel.";
+            if(bebop_tables::get_option('bebop_'.$extension['name'].'_provider') == "on") {
+                echo '<li><a href="?oer=' . $extension['name'] . '">'.ucfirst($extension['displayname']).'</a></li>';         
+                $activeExtensions[] = $extension['name'];
+            }
+        }
+		if( count($activeExtensions) == 0 ) {
+			echo "No extensions are currently active. Please activate them in the bebop OER provides admin panel.";
 		}
         ?>
     </ul>
 </div>
 <br/><br/>
-<div>
 
 <?php
 if(isset($_GET['oer'])){
@@ -37,4 +39,3 @@ if(isset($_GET['oer'])){
     include(WP_PLUGIN_DIR."/bebop/extensions/".$_GET['oer']."/templates/user_settings.php");
 }
 ?>
-</div>
