3e7ca8d17f7361c0351974b80bacffaa31387ea7
David Whitehead <dwhitehead@lincoln.ac.uk>
2012-07-09 14:28:39 +0100

    The new changes for seeing and deleting user stuff on the back end of the youtube

--- extensions/youtube/templates/admin_settings.php
+++ extensions/youtube/templates/admin_settings.php
@@ -7,45 +7,34 @@
 
 <?php
 
- $arraySwitches = array(
-        'bebop_youtube_hide_sitewide',
-        'bebop_youtube_album'
-    );
+$arraySwitches = array(
+					   'bebop_youtube_hide_sitewide',
+   					   'bebop_youtube_album'
+  					  );
 
-  if ($_POST) {
-  	
-	if($_POST['bebop_youtube_maximport']){
-    	update_site_option('bebop_youtube_maximport', trim(strip_tags(strtolower($_POST['bebop_youtube_maximport']))));
-    	update_site_option('bebop_youtube_setup', true);
+if ($_POST) {
+   	update_site_option('bebop_youtube_maximport', trim(strip_tags(strtolower($_POST['bebop_youtube_maximport']))));
+   	update_site_option('bebop_youtube_setup', true);
       
-    	foreach($arraySwitches as $switch){
-        	update_site_option($switch, trim(strip_tags(strtolower($_POST[$switch]))));    
-      	}  
-      	echo '<div>Settings Saved</div>';
-	}
-   }
+   	foreach($arraySwitches as $switch){
+       	update_site_option($switch, trim(strip_tags(strtolower($_POST[$switch]))));    
+   	}  
+   	echo '<div>Settings Saved</div>';
+}
+  //remove the user
+if( isset( $_GET['reset_user_id'] ) ) {
+	$user_id = trim($_GET['reset_user_id']);
+	bebop_tables::remove_user_from_provider($user_id, 'youtube');
+}
 ?>
-
-<div class="bebop_info_box">Youtube settings</div>
 
 <form method="post" action="">
 	<table class="bebop_settings_table" cellspacing="0">          
 	
-		<tr class="header">
-			<td colspan="2">Header</td>
-		</tr>
-		
-		<tr>
-			<td>Album</td>
-			<td><input class="switch icons" type="checkbox" name="bebop_youtube_album" id="buddystream_youtube_album"/></td>
-		</tr>
-		
-		<tr class="odd">
-			<td>Hide sitewide?</td>
-			<td><input class="switch icons" type="checkbox" name="bebop_youtube_hide_sitewide" id="bebop_youtube_hide_sitewide"/></td>
-		</tr>
-		
-		<tr>
+
+			<th colspan='2'>Youtube settings</th>
+
+<tr>
 			<td>Max import:</td>
 			<td><input type="text" name="bebop_youtube_maximport" value="<?php echo get_site_option('bebop_youtube_maximport'); ?>" size="5" /></td>
 		</tr>
@@ -60,6 +49,7 @@
 		<th>Username</th>
 		<th>User email</th>
 		<th>Youtube Channel</th>
+		<th>Options</th>
 	</tr>
 	<?php
 	$user_metas = bebop_tables::get_user_ids_from_meta_name('bebop_youtube_username');	
@@ -72,13 +62,9 @@
 			<td>" . bebop_tables::sanitise_element($this_user->user_login) . "</td>
 			<td>" . bebop_tables::sanitise_element($this_user->user_email) . "</td>
 			<td>" . bebop_tables::sanitise_element(bebop_tables::get_user_meta_value( $user->user_id, 'bebop_youtube_username' ) ) . "</td>
-			<td>
-				<form method='post' action=''>
-				<input type='submit' name='delsub' value='Delete'>
-				</form>
-			</td>
+			<td><a href='?page=bebop_youtube&reset_user_id=" . bebop_tables::sanitise_element($user->user_id) . "'>Reset User</a></td>
 		</tr>";
 	}
-	?>
+?>
 	<!-- <End bebop_table -->
 </table>
