1f545b7ab369f07ae2a2d6b07380d128135eaf0a
DaleMckeown <@dalemac.09@googlemail.com>
2012-07-09 10:59:32 +0100

    Latest changes

--- core/bebop_tables.php
+++ core/bebop_tables.php
@@ -21,8 +21,8 @@ class bebop_tables
 	function remove_user_from_provider($user_id, $provider) {
 		global $wpdb, $bp;
 		
-		if( $wpdb->get_results( "DELETE FROM {$bp->activity->table_name} WHERE user_id = '" . $wpdb->escape($user_id) . "' AND type = '" . $wpdb->escape($provider) . "'") ) {
-			if($wpdb->get_results( "DELETE FROM " . $wpdb->base_prefix . "bp_bebop_user_meta  WHERE user_id = '" . $wpdb->escape($user_id) . "' AND meta_type = '" . $wpdb->escape($provider) . "' LIMIT 1" ) ) {
+		if($wpdb->get_results( "DELETE FROM " . $wpdb->base_prefix . "bp_bebop_user_meta  WHERE user_id = '" . $wpdb->escape($user_id) . "' AND meta_type = '" . $wpdb->escape($provider) . "'") ) {
+			if( $wpdb->get_results( "DELETE FROM " . $bp->activity->table_name ." WHERE user_id = '" . $wpdb->escape($user_id) . "' AND component = '" . $wpdb->escape($provider) . "' AND type = '" . $wpdb->escape($provider) . "'") ) {
 				return true;
 			}
 			else {
