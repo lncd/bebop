b43dfb6d51aef973f35a9be1830efde99adac274
David Whitehead <dwhitehead@lincoln.ac.uk>
2012-07-10 14:52:32 +0100

    New filter for incrementing the current amount

--- core/bebop_filters.php
+++ core/bebop_filters.php
@@ -2,21 +2,31 @@
 //Filters modify data and return the values to the original function.
 class bebop_filters
 {
+	
+	//This grabs the day counter for that specific extension and increases it by one.	
+	public function day_increase($extension, $userId){
+        $database_count = bebop_tables::get_user_meta_value($userId, 'bebop_' . $extension . '_daycounter');
+        $new_count = $database_count+1;
+	
+        bebop_tables::update_user_meta($userId, $extension, 'bebop_' . $extension . '_daycounter', $new_count);
+    }
+	
+	//This checks to see if the limit for the amount of imports has been maxed for the day.
 	public function import_limit_reached($extension, $userId){
 
 		//different day ot no day set, set the day and the counter to 0;
-		if (bebop_tables::get_user_meta_value($userId, 'twitter', 'bebop_' . $extension . '_counterdate', 1) != date('dmy')) {
-			bebop_tables::update_user_meta($userId, 'twitter', 'bebop_' . $extension . '_daycounter', '0');
-			bebop_tables::update_user_meta($userId, 'twitter', 'bebop_' . $extension . '_counterdate', date('dmy'));
+		if (bebop_tables::get_user_meta_value($userId, 'bebop_' . $extension . '_counterdate') != date('dmy')) {
+			bebop_tables::update_user_meta($userId, $extension, 'bebop_' . $extension . '_daycounter', '0');
+			bebop_tables::update_user_meta($userId, $extension, 'bebop_' . $extension . '_counterdate', date('dmy'));
 		}
 		
-		//max items per day
-		if (bebop_tables::get_option_value('bebop_' . $extension . '_maximport')) {
-			if (bebop_tables::get_user_meta($userId, 'bebop_' . $extension . '_daycounter',1) <= bebop_tables::get_option_value('bebop_' . $extension . '_maximport')) {
+		//max items per day * < shud be false return*
+		if (bebop_tables::get_option_value('bebop_' . $extension . '_maximport')) {			
+			if (bebop_tables::get_user_meta_value($userId, 'bebop_' . $extension . '_daycounter',1) < bebop_tables::get_option_value('bebop_' . $extension . '_maximport')) {
 				return false;
 			}
 		}
-		else{
+		else{			
 			return false;
 		}
 		return true;
