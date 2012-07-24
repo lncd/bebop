42818aed4f907dbf97001d7814b0c1ef3a64c2a4
DaleMckeown <@dalemac.09@googlemail.com>
2012-07-24 10:38:12 +0100

    oops

--- core/templates/admin/bebop_admin_menu.php
+++ core/templates/admin/bebop_admin_menu.php
@@ -3,5 +3,5 @@
 	<a id='bebop_general' href='?page=bebop_admin_settings' <?php if ( $_GET['page'] == 'bebop_admin_settings' ) {echo 'class="activetab"';}?>>General Settings</a>
 	<a id='bebop_oer_providers' href='?page=bebop_oer_providers' <?php if ( $_GET['page'] == 'bebop_oer_providers' ) {echo 'class="activetab"';}?>>OER Providers</a>
 	<a id='bebop_error_log' href='?page=bebop_error_log' <?php if ( $_GET['page'] == 'bebop_error_log' ) {echo 'class="activetab"';}?>>Error Log</a>
-	<a id='bebop_general_log' href='?page=bebop_general_log' <?php if ( $_GET['page'] == 'bebop_general_log' ) {echo 'class="activetab"'; ?>>General Log</a>
+	<a id='bebop_general_log' href='?page=bebop_general_log' <?php if ( $_GET['page'] == 'bebop_general_log' ) {echo 'class="activetab"';}?>>General Log</a>
 </div>
