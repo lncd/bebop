2e7b36a30dab28ccc2103cc72e68c01ee4483525
DaleMckeown <@dalemac.09@googlemail.com>
2012-07-31 12:55:47 +0100

    changing buttons, implementing admin oer viewer

--- core/templates/admin/bebop-oers.php
+++ core/templates/admin/bebop-oers.php
@@ -7,27 +7,35 @@
 	if ( isset( $_GET['type'] ) ) {
 		if ( strtolower( strip_tags( $_GET['type'] == 'unverified' ) ) ) {
 			$type = 'unverified';
-			$message = 'unverified';
+			$message = 'These OERs have not been approved to be displayed in owners activity streams.';
 		}
 		else if ( strtolower( strip_tags( $_GET['type'] == 'verified' ) ) ) {
 			$type = 'verified';
-			$message = 'verified';
+			$message = 'These OERs are currently being displayed their owner\'s activity streams.';
 		}
 		else if ( strtolower( strip_tags( $_GET['type'] == 'deleted' ) ) ) {
 			$type = 'deleted';
-			$message = 'deleted';
+			$message = 'These OERs are not in the activity stream and have been marked as deleted by the owner.';
 		}
 	}
 	else {
 		$type = 'verified';
-		$message = 'verified';
+		$message = 'These OERs are currently being displayed their owner\'s activity streams.';
 	}
+	
+	echo '<div class="standard_class">';
+	echo '<a class="options_button" href="' . $_SERVER['PHP_SELF'] . '?' . http_build_query( $_GET ) . '&type=unverified">Unverified OERs</a>';
+	echo '<a class="options_button" href="' . $_SERVER['PHP_SELF'] . '?' . http_build_query( $_GET ) . '&type=verified">Verified OERs</a>';
+	echo '<a class="options_button" href="' . $_SERVER['PHP_SELF'] . '?' . http_build_query( $_GET ) . '&type=deleted">Deleted OERs</a>';
+	echo '</div>';
 	
 	$oers = bebop_tables::admin_fetch_oer_data( $type );
 	
 	if ( count( $oers ) > 0 ) {
+		echo '<div class="standard_class">';
 		echo '<h4>' . ucfirst( $type ) . ' OERs</h4>';
 		echo $message;
+		echo '</div>';
 		
 		echo '<table class="bebop_table">
 			<tr class="nodata">
@@ -55,7 +63,10 @@
 		}
 	}
 	else {
+		echo '<div class="standard_class">';
+		echo '<h4>' . ucfirst( $type ) . ' OERs</h4>';
 		echo '<p>No ' . $type . ' oers exist in the oer manager.</p>';
+		echo '</div>';
 	}
 		
 	?>
