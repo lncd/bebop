22f314230ab858d3f6c5c30b4982e446c9ddc0f4
DaleMckeown <dalemac.09@googlemail.com>
2012-08-10 13:51:12 +0100

    i really think the import bug is fixed now

--- import.php
+++ import.php
@@ -33,7 +33,7 @@ if ( ! empty( $importers[0] ) ) {
 		if ( bebop_tables::get_option_value( 'bebop_' . $extension . '_provider' ) == 'on' ) {
 			if ( file_exists( WP_PLUGIN_DIR . '/bebop/extensions/' . strtolower( $extension ) . '/import.php' ) ) {
 			
-				include_once( WP_PLUGIN_DIR . '/bebop/extensions/' . $extension . '/import.php' );
+				include_once( WP_PLUGIN_DIR . '/bebop/extensions/' . strtolower( $extension ) . '/import.php' );
 				if ( function_exists( 'bebop_' . strtolower( $extension ) . '_import' ) ) {
 					//call the import function, and pass in the extension name.
 					$return_array[] = call_user_func( 'bebop_' . strtolower( $extension ) . '_import', $extension );
