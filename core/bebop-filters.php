d0a8d2a28714a09c0479cb168a8e0f69e99da4f8
DaleMckeown <dalemac.09@googlemail.com>
2012-09-21 16:35:09 +0100

    git up with ' in db's - just gonna switch characters

--- core/bebop-filters.php
+++ core/bebop-filters.php
@@ -3,10 +3,10 @@ class bebop_filters {
 	//Increment the day counter
 	public function day_increase( $extension, $user_id, $username ) {
 		$maximport_value = bebop_tables::get_option_value( 'bebop_' . $extension . '_maximport' );
-		$user_count = bebop_tables::get_user_meta_value( $user_id, 'bebop_' . $extension . '_' . $username . '_daycounter' );
+		$user_count = (INT)bebop_tables::get_user_meta_value( $user_id, 'bebop_' . $extension . '_' . $username . '_daycounter' );
 		
-		var_dump($user_count);
-		if ( ! $user_count === false ) {
+		
+		if ( ( ! empty( $user_count ) ) || ( is_numeric( $user_count ) ) ) {
 			if ( ( empty( $maximport_value ) || $maximport_value === 0 ) || ( $maximport_value > $user_count ) ) {
 				$new_count = $user_count + 1;
 				bebop_tables::update_user_meta( $user_id, $extension, 'bebop_' . $extension . '_' . $username . '_daycounter', $new_count );
