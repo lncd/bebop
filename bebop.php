62ed263c68940bd05e2cc0bf8d0945e62709b8d0
DaleMckeown <dalemac.09@googlemail.com>
2013-02-25 10:04:16 +0000

    git

--- bebop.php
+++ bebop.php
@@ -15,7 +15,6 @@ Credits: BuddySteam - buddystream.net
 // This plugin is intended for use on BuddyPress only.
 // http://buddypress.org/
 
-
 /****************************************************************************
 // This program is distributed in the hope that it will be useful, but		*
 // WITHOUT ANY WARRANTY; without even the implied warranty of				*
